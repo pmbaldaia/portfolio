3e8de23c300f258f5decaec5886418aca81b40a5
pmbaldaia <pmbaldaia@outlook.com>
2023-06-13 12:10:11 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import Header from "../src/components/Header";
-import About from "../src/components/About";
-import Experience from "../src/components/Experience";
-import Projects from "../src/components/Projects";
-import Contact from "../src/components/Contact";
-import Footer from "../src/components/Footer";
-import Loading from "../src/components/Loading/loading";
-import NavBar from "../src/components/NavBar";
+import Header from "./components/Header.js";
+import About from "./components/About.js";
+import Experience from "./components/Experience.js";
+import Projects from "./components/Projects.js";
+import Contact from "./components/Contact.js";
+import Footer from "./components/Footer.js";
+import Loading from "./components/Loading/loading.js";
+import NavBar from "./components/NavBar.js";
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const color = "blue";
