a0da34bb9318e7ae33caa45e3322714f8d076d41
pmbaldaia <32077414+pmbaldaia@users.noreply.github.com>
2023-09-12 17:57:12 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>VAMOS VER SE É DESTA SE FICA FEITO</p>
+        <p>VAMOS VER SE É DESTA QUE FICA FEITO</p>
       </header>
     </div>
   );
